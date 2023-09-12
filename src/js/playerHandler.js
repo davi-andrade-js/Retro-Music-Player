3aa24a734d8e277b1700696cfcb864087fec666b
Davi Andrade <dv.andrade04@gmail.com>
2023-09-12 09:27:06 -0300

    some style changes

--- src/js/playerHandler.js
+++ src/js/playerHandler.js
@@ -1,4 +1,13 @@
+import songs from "./songs.js";
+
+const screens = document.getElementById("screens");
+const songTitle = document.querySelector(".songTitle");
+const songAuthor = document.querySelector(".songAuthor");
+const songGif = document.querySelector(".songGif");
+
 export function playerHandler() {
-  const screens = document.getElementById("screens");
   screens.style.display = "flex";
+  songTitle.textContent = songs[0].name;
+  songAuthor.textContent = songs[0].author;
+  songGif.src = songs[0].gif;
 }
--- src/js/songs.js
+++ src/js/songs.js
@@ -3,6 +3,6 @@ export default [
     scr: "../songs/wave.mp3",
     name: "Wave",
     author: "Tom Jobim",
-    gif: "https://media3.giphy.com/media/OMFfLpauGoT4c/giphy.gif?cid=ecf05e47ndjmzbpr5b4n2hxvezb4acdvmrtmcdeafwgojrmp&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    gif: "./images/wave.gif",
   },
 ];
