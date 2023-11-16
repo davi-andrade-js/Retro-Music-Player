a2e3862cfcbf78d7d6435eaed9e63d2dc351cc2c
Davi Andrade <dv.andrade04@gmail.com>
2023-11-16 17:01:08 -0300

    wallpapers switch working

--- src/js/my-computer.js
+++ src/js/my-computer.js
@@ -1,7 +1,18 @@
 const computerScreen = document.getElementById("computerScreen");
+const computerSection = document.getElementById("computerSection");
 
 export function renderWallpapers() {
-  const computerSection = document.getElementById("computerSection");
   computerSection.style.display = "flex";
   computerScreen.style.display = "block";
 }
+
+const wallpapers = document.querySelectorAll(".wallpaper");
+
+wallpapers.forEach((wallpaper) => {
+  wallpaper.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    const imageUrl = wallpaper.getAttribute("src");
+    body.style.backgroundImage = `url(${imageUrl})`;
+    computerSection.style.display = "none";
+  });
+});
