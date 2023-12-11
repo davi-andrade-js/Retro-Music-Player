34d82b543be9a28bf9ac2035c7584ca9da68af3f
Davi Andrade <dv.andrade04@gmail.com>
2023-12-11 15:34:43 -0300

    clicl sound working

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,41 @@
+import { playClickSound } from "./script.js";
+
 export function renderConfig() {
   const configSection = document.getElementById("configSection");
   const configScreen = document.getElementById("configScreen");
   configSection.style.display = "flex";
   configScreen.style.display = "block";
 }
+
+const clickSound = document.getElementById("clickSound");
+const soundOnBtn = document.getElementById("soundOnBtn");
+const soundOffBtn = document.getElementById("soundOffBtn");
+
+let clickSoundOn = localStorage.getItem("click") ?? true;
+
+if (clickSoundOn === "true" || clickSoundOn === true) {
+  soundOnBtn.style.backgroundColor = "#00ff00";
+  clickSound.muted = false;
+} else {
+  soundOffBtn.style.backgroundColor = "#ff0000";
+  clickSound.muted = true;
+}
+
+soundOnBtn.addEventListener("click", () => {
+  playClickSound();
+  clickSound.muted = false;
+  soundOnBtn.style.backgroundColor = "#00ff00";
+  soundOffBtn.style.backgroundColor = "#c0c0c0";
+  clickSoundOn = true;
+
+  localStorage.setItem("click", clickSoundOn);
+});
+
+soundOffBtn.addEventListener("click", () => {
+  clickSound.muted = true;
+  soundOffBtn.style.backgroundColor = "#ff0000";
+  soundOnBtn.style.backgroundColor = "#c0c0c0";
+  clickSoundOn = false;
+
+  localStorage.setItem("click", clickSoundOn);
+});
